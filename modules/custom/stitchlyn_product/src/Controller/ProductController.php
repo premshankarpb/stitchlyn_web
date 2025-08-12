a02159689fa1251a602aaebd6f12a973604ce15f
Prem <prem.shankar@stplc.info>
2025-08-12 20:56:16 +0530

    added the Product, quotation, invoice content models and releavant modules

---
+++ modules/custom/stitchlyn_product/src/Controller/ProductController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\stitchlyn_product\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\NodeInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class ProductController extends ControllerBase {
+
+  /**
+   * Page title: Product name.
+   */
+  public function title(NodeInterface $node): string {
+    // 404 if not a Product.
+    if ($node->bundle() !== 'product') {
+      throw new NotFoundHttpException();
+    }
+    return $node->label();
+  }
+
+  /**
+   * Render the product node using the node view builder.
+   */
+  public function view(NodeInterface $node) {
+    if ($node->bundle() !== 'product') {
+      throw new NotFoundHttpException();
+    }
+
+    // Choose a view mode. 'full' or create a custom one like 'product_detail'.
+    $view_mode = 'full';
+
+    $build = $this->entityTypeManager()
+      ->getViewBuilder('node')
+      ->view($node, $view_mode);
+
+    // Add cacheability (Drupal will merge this with node cache metadata).
+    $build['#cache']['contexts'][] = 'user.permissions';
+
+    return $build;
+  }
+
+}
