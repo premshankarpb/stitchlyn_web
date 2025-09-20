7072b3369f74310e3c24ec8984c1a03ece09370f
Prem <prem.shankar@stplc.info>
2025-09-20 23:32:50 +0530

    added the erp client admin form

---
+++ modules/custom/stitchlyn_basic/src/Form/ERPClientSettingsForm.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\stitchlyn_basic\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\file\Entity\File;
+
+class ERPClientSettingsForm extends ConfigFormBase {
+
+  protected function getEditableConfigNames() {
+    return ['stitchlyn_basic.erp_settings'];
+  }
+
+  public function getFormId() {
+    return 'stitchlyn_basic_erp_settings_form';
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('stitchlyn_basic.erp_settings');
+
+    $form['client_name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Client Name'),
+      '#default_value' => $config->get('client_name'),
+      '#required' => TRUE,
+    ];
+
+    $form['client_address'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Client Address'),
+      '#default_value' => $config->get('client_address'),
+    ];
+
+    $form['client_gst'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Client GST Number'),
+      '#default_value' => $config->get('client_gst'),
+    ];
+
+    $form['client_logo'] = [
+      '#type' => 'managed_file',
+      '#title' => $this->t('Client Logo'),
+      '#upload_location' => 'public://erp_logo/',
+      '#default_value' => $config->get('client_logo'),
+      '#upload_validators' => [
+        'file_validate_extensions' => ['png jpg jpeg gif'],
+      ],
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    if ($fid = $form_state->getValue('client_logo')) {
+      $file = File::load($fid[0]);
+      if ($file) {
+        $file->setPermanent();
+        $file->save();
+      }
+    }
+
+    $this->config('stitchlyn_basic.erp_settings')
+      ->set('client_name', $form_state->getValue('client_name'))
+      ->set('client_address', $form_state->getValue('client_address'))
+      ->set('client_gst', $form_state->getValue('client_gst'))
+      ->set('client_logo', $form_state->getValue('client_logo'))
+      ->save();
+  }
+}
