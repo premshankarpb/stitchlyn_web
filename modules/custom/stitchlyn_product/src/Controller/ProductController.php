e095e0f99043cbee3018c95d5e95bcf91056522b
ronz-sam <ron.sam.here@gmail.com>
2025-09-21 14:16:25 +0530

    Add/Edit Product, Product listing update

--- modules/custom/stitchlyn_product/src/Controller/ProductController.php
+++ modules/custom/stitchlyn_product/src/Controller/ProductController.php
@@ -5,6 +5,11 @@ namespace Drupal\stitchlyn_product\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\node\NodeInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Drupal\Core\Url;
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Form\FormBuilderInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Drupal\node\Entity\Node;
 
 class ProductController extends ControllerBase {
 
@@ -38,6 +43,53 @@ class ProductController extends ControllerBase {
     $build['#cache']['contexts'][] = 'user.permissions';
 
     return $build;
+  }
+
+  /**
+   * Render the add form for a work_order node.
+   */
+
+  public function add() {
+    // Check access: user must have permission to create 'work_order' content.
+    $access = \Drupal::currentUser()->hasPermission('create work_order content');
+    if (!$access) {
+      throw new AccessDeniedHttpException();
+    }
+
+    // Create a new unsaved node entity of type 'work_order'.
+    $node = Node::create(['type' => 'product']);
+
+    // Use the entity form builder to render the 'add' form.
+    return \Drupal::service('entity.form_builder')->getForm($node, 'default');
+  }
+
+  /**
+   * Render the edit form for a work_order node.
+   */
+  public function edit(NodeInterface $node) {
+    if ($node->bundle() !== 'product') {
+      throw new NotFoundHttpException();
+    }
+
+    if (!$node->access('update')) {
+      throw new AccessDeniedHttpException();
+    }
+
+    // Use the entity form builder to get the edit form.
+    $form = \Drupal::service('entity.form_builder')->getForm($node, 'edit');
+
+    return $form;
+  }
+
+  /**
+   * Access control for the controller routes.
+   */
+  public function access(NodeInterface $node) {
+    if ($node->bundle() !== 'product') {
+      return AccessResult::forbidden();
+    }
+
+    return $node->access('update') ? AccessResult::allowed() : AccessResult::forbidden();
   }
 
 }
