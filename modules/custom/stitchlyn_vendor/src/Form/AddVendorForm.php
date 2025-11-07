97c66c2dd92e8673b9551c8a872637e0829f65fb
ronz-sam <ron.sam.here@gmail.com>
2025-11-07 22:35:24 +0530

    Vendor type field created, add form updated, listing view updated with new filter

--- modules/custom/stitchlyn_vendor/src/Form/AddVendorForm.php
+++ modules/custom/stitchlyn_vendor/src/Form/AddVendorForm.php
@@ -6,6 +6,8 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\user\Entity\User;
 use Drupal\profile\Entity\Profile;
+use Drupal\taxonomy\Entity\Term;
+use Drupal\taxonomy\Entity\Vocabulary;
 
 class AddVendorForm extends FormBase {
 
@@ -69,12 +71,37 @@ class AddVendorForm extends FormBase {
       '#title' => $this->t('About'),
     ];
 
+    $vocabulary = 'status';
+    $terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($vocabulary);
+    $options = [];
+
+    foreach ($terms as $term) {
+      $options[$term->tid] = $term->name;
+    }
+
     $form['field_status'] = [
-      '#type' => 'entity_autocomplete',
+      '#type' => 'select',
       '#title' => $this->t('Status'),
-      '#target_type' => 'taxonomy_term',
-      '#selection_settings' => ['target_bundles' => ['status']],
+      '#options' => $options,
+      '#empty_option' => $this->t('- Select a status -'),
+      '#required' => TRUE,
     ];
+
+    $vocabulary_2 = 'vendor_type';
+    $vendor_terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($vocabulary_2);
+    $options_vendor_type = [];
+
+    foreach ($vendor_terms as $term) {
+      $options_vendor_type[$term->tid] = $term->name;
+    }
+
+    $form['field_vendor_type'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Vendor Type'),
+      '#options' => $options_vendor_type,
+      '#empty_option' => $this->t('- Select a Type -'),
+      '#required' => TRUE,
+    ];    
 
     $form['submit'] = [
       '#type' => 'submit',
@@ -104,11 +131,12 @@ class AddVendorForm extends FormBase {
     $profile->set('field_vendor_name', $form_state->getValue('field_vendor_name'));
     $profile->set('field_contact_person', $form_state->getValue('field_contact_person'));
     $profile->set('field_phone_number', $form_state->getValue('field_phone_number'));
-    $profile->set('field_vendor_location', $form_state->getValue('field_vendor_location'));
+    //$profile->set('field_vendor_location', $form_state->getValue('field_vendor_location'));
     $profile->set('field_billing_address', $form_state->getValue('field_billing_address'));
     $profile->set('field_gst_number', $form_state->getValue('field_gst_number'));
-    $profile->set('field_about', $form_state->getValue('field_about'));
+    $profile->set('field_remarks', $form_state->getValue('field_about'));
     $profile->set('field_status', $form_state->getValue('field_status'));
+    $profile->set('field_vendor_type', $form_state->getValue('field_vendor_type'));
 
     $profile->save();
 
