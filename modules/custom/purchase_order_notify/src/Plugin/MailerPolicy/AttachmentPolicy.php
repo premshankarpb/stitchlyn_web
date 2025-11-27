7420650ec7873c25ae922b0284cd3eb2831089fe
Premshankar <premshankar.p.b@gmail.com>
2025-11-27 22:01:03 +0530

    fixed the symphony mailer

--- modules/custom/purchase_order_notify/src/Plugin/MailerPolicy/AttachmentPolicy.php
+++ modules/custom/purchase_order_notify/src/Plugin/MailerPolicy/AttachmentPolicy.php
@@ -29,24 +29,22 @@ class AttachmentPolicy extends EmailPolicyBase {
    * {@inheritdoc}
    */
   public function alterEmail(Email $email, array $message) {
+
+    \Drupal::logger('po_mail_debug')->info('<pre>' . print_r($message, TRUE) . '</pre>');
+
     if (empty($message['params']['attachment'])) {
-      return;
+        \Drupal::logger('po_mail_debug')->warning('Attachment param missing');
+        return;
     }
 
     $attachment = $message['params']['attachment'];
+    \Drupal::logger('po_mail_debug')->info('Attachment detected');
 
-    $file_content = $attachment['filecontent'] ?? NULL;
-    $filename     = $attachment['filename'] ?? 'attachment.pdf';
-    $filemime     = $attachment['filemime'] ?? 'application/pdf';
-
-    if ($file_content) {
-      // Add as PDF attachment.
-      $email->attach(
-        $file_content,
-        $filename,
-        $filemime
-      );
-    }
-  }
+    $email->attach(
+        $attachment['filecontent'],
+        $attachment['filename'],
+        $attachment['filemime']
+    );
+	}
 
 }
