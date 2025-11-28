5c3519d97573907cb586af5d55d5a06aab0822fd
Premshankar <premshankar.p.b@gmail.com>
2025-11-29 00:44:20 +0530

    aligned the Po mail

--- modules/custom/purchase_order_notify/src/Plugin/MailerPolicy/AttachmentPolicy.php
+++ modules/custom/purchase_order_notify/src/Plugin/MailerPolicy/AttachmentPolicy.php
@@ -4,12 +4,13 @@ namespace Drupal\purchase_order_notify\Plugin\MailerPolicy;
 
 use Drupal\symfony_mailer\Plugin\EmailPolicyBase;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Part\DataPart;
 
 /**
  * @EmailPolicy(
  *   id = "purchase_order_notify_attachment",
  *   label = @Translation("PO/Quotation Attachment Handler"),
- *   description = @Translation("Adds PDF attachments from hook_mail()."),
+ *   description = @Translation("Adds PDF attachments from mail params."),
  *   weight = 100
  * )
  */
@@ -19,6 +20,10 @@ class AttachmentPolicy extends EmailPolicyBase {
    * {@inheritdoc}
    */
   public function applies(array $message) {
+
+    // Debug to confirm plugin activation
+    \Drupal::logger('po_mail_debug')->info('AttachmentPolicy::applies triggered');
+
     return !empty($message['params']['attachment']);
   }
 
@@ -27,22 +32,21 @@ class AttachmentPolicy extends EmailPolicyBase {
    */
   public function alterEmail(Email $email, array $message) {
 
-    \Drupal::logger('po_mail_debug')->info('<pre>' . print_r($message, TRUE) . '</pre>');
+    \Drupal::logger('po_mail_debug')->info('<pre>'. print_r($message['params'], TRUE) .'</pre>');
 
     if (empty($message['params']['attachment'])) {
-      \Drupal::logger('po_mail_debug')->warning('Attachment param missing');
+      \Drupal::logger('po_mail_debug')->warning('Attachment missing in params');
       return;
     }
 
-    $attachment = $message['params']['attachment'];
+    $file = $message['params']['attachment'];
 
-    \Drupal::logger('po_mail_debug')->info('Attachment detected');
+    $email->addPart(new DataPart(
+      $file['filecontent'],      // binary PDF
+      $file['filename'],         // e.g. po-19.pdf
+      $file['filemime']          // application/pdf
+    ));
 
-    $email->attach(
-      $attachment['filecontent'],
-      $attachment['filename'],
-      $attachment['filemime']
-    );
+    \Drupal::logger('po_mail_debug')->info('PDF attachment added successfully.');
   }
-
 }
