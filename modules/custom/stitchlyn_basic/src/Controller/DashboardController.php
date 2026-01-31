8bcc1d8aa861c52d028e7ec3e23dd3db53de1dff
Premshankar <premshankar.p.b@gmail.com>
2026-01-31 15:48:56 +0530

    dynamic numbering on manager dashboard

--- modules/custom/stitchlyn_basic/src/Controller/DashboardController.php
+++ modules/custom/stitchlyn_basic/src/Controller/DashboardController.php
@@ -149,7 +149,7 @@ class DashboardController extends ControllerBase {
       }
 
       // ---- Monthly production ----
-      $duedata_date_format = DrupalDateTime::createFromTimestamp($due_date_raw);
+      $duedata_date_format = new DrupalDateTime($due_date_raw);
       if ($duedata_date_format->format('Y-m') === $current_month) {
         $monthly['total'] += $qty;
         switch ($status) {
